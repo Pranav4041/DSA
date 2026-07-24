accdb4c1af42b09890d501b8b1d26b59882c3646
Pranav4041 <168201896+Pranav4041@users.noreply.github.com>
2026-07-24 14:11:30 +0530

    Time: 4 ms (49.41%) | Memory: 118 MB (82.32%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -42,5 +42,12 @@ public:
             right=right->next;
         }
         return result;
+        while(curr)
+        {
+            ListNode*next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
     }
 };
