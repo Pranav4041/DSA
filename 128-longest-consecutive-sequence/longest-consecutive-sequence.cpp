635b082676ffbe15be967e6b1f09221be7ce9789
Pranav4041 <168201896+Pranav4041@users.noreply.github.com>
2026-07-26 23:38:54 +0530

    Time: 100 ms (14.23%) | Memory: 92.9 MB (5.68%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    unordered_map<int, int> parent;
-    unordered_map<int, int> size;
+    vector<int> parent, sz;
 
     int find(int x) {
         while (parent[x] != x) {
@@ -14,30 +13,36 @@ public:
     void unite(int x, int y) {
         int rx = find(x), ry = find(y);
         if (rx == ry) return;
-        if (size[rx] < size[ry]) swap(rx, ry);
+        if (sz[rx] < sz[ry]) swap(rx, ry);
         parent[ry] = rx;
-        size[rx] += size[ry];
+        sz[rx] += sz[ry];
     }
 
     int longestConsecutive(vector<int>& nums) {
         if (nums.empty()) return 0;
 
-        for (int n : nums) {
-            if (parent.find(n) == parent.end()) {
-                parent[n] = n;
-                size[n] = 1;
+        // coordinate compression: map each number to an index
+        unordered_map<int, int> index;
+        int n = 0;
+        for (int num : nums) {
+            if (index.find(num) == index.end()) {
+                index[num] = n++;
             }
         }
 
-        for (int n : nums) {
-            if (parent.find(n + 1) != parent.end()) {
-                unite(n, n + 1);
+        parent.resize(n);
+        sz.assign(n, 1);
+        for (int i = 0; i < n; i++) parent[i] = i;
+
+        for (int num : nums) {
+            if (index.count(num + 1)) {
+                unite(index[num], index[num + 1]);
             }
         }
 
         int maxSize = 0;
-        for (auto& [num, _] : parent) {
-            maxSize = max(maxSize, size[find(num)]);
+        for (int i = 0; i < n; i++) {
+            maxSize = max(maxSize, sz[find(i)]);
         }
         return maxSize;
     }
