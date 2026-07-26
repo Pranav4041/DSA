65f380327d145aabd0f38caf0e94bf9ddb5a86ab
Pranav4041 <168201896+Pranav4041@users.noreply.github.com>
2026-07-26 23:40:37 +0530

    Time: 84 ms (41.05%) | Memory: 88.9 MB (67.35%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,49 +1,19 @@
 class Solution {
 public:
-    vector<int> parent, sz;
-
-    int find(int x) {
-        while (parent[x] != x) {
-            parent[x] = parent[parent[x]]; // path compression
-            x = parent[x];
-        }
-        return x;
-    }
-
-    void unite(int x, int y) {
-        int rx = find(x), ry = find(y);
-        if (rx == ry) return;
-        if (sz[rx] < sz[ry]) swap(rx, ry);
-        parent[ry] = rx;
-        sz[rx] += sz[ry];
-    }
-
     int longestConsecutive(vector<int>& nums) {
-        if (nums.empty()) return 0;
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        int longest = 0;
 
-        // coordinate compression: map each number to an index
-        unordered_map<int, int> index;
-        int n = 0;
-        for (int num : nums) {
-            if (index.find(num) == index.end()) {
-                index[num] = n++;
+        for (int num : numSet) {
+            // only start counting if num is the beginning of a sequence
+            if (numSet.find(num - 1) == numSet.end()) {
+                int length = 1;
+                while (numSet.find(num + length) != numSet.end()) {
+                    length++;
+                }
+                longest = max(longest, length);
             }
         }
-
-        parent.resize(n);
-        sz.assign(n, 1);
-        for (int i = 0; i < n; i++) parent[i] = i;
-
-        for (int num : nums) {
-            if (index.count(num + 1)) {
-                unite(index[num], index[num + 1]);
-            }
-        }
-
-        int maxSize = 0;
-        for (int i = 0; i < n; i++) {
-            maxSize = max(maxSize, sz[find(i)]);
-        }
-        return maxSize;
+        return longest;
     }
 };
