005fcc5c595e252a8823dd4faff366d8705da5a1
Pranav4041 <168201896+Pranav4041@users.noreply.github.com>
2026-07-30 12:18:17 +0530

    Time: 43 ms (83.75%) | Memory: 29 MB (92.76%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -4,7 +4,7 @@ public:
         
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-        for(int i=0;i<nums.size()-2;i++)
+        for(int i=0;i<nums.size();i++)
         {
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
